5636a8c5d046015f1092b675c6595981f7d85949
Mykola Malashchuk <mykola_malashchuk@epam.com>
2021-09-21 17:56:04 +0300

    change env file

--- src/api.js
+++ src/api.js
@@ -1,16 +1,20 @@
 import axios from 'axios';
-import { encode as base64_encode} from 'base-64';
 
 const API_URL = 'http://127.0.0.1:5000/api/v1';
-const token = base64_encode(`user:${process.env.REACT_APP_GIT_HUB_TOKEN}`)
-const headers = {
-    'Content-Type': 'application/json',
-    'Authorization': `Basic: ${token}`
+
+const config = {
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    auth: {
+        username: process.env.REACT_APP_GIT_HUB_USER,
+        password: process.env.REACT_APP_GIT_HUB_TOKEN
+    }
 }
   
 
 export const Templates = {
     get: (params) => axios.get(`${API_URL}/templates`, params),
     getOne: (slag, params) => axios.get(`${API_URL}/templates/${slag}`, params),
-    send: (body) => axios.post(`${API_URL}/git-modules`, body, {headers: headers})
+    send: (body) => axios.post(`${API_URL}/git-modules`, body, config)
 }
